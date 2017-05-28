ef4d2990655edb304fc9df22b39c4a4018162370
Lukasz Ludwin <gorli@interia.pl>
2017-05-28 16:11:25 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+﻿var a = 0,
+	h = 0,
+	triangleArea;
+
+var a = prompt('Enter triangle base');
+var h = prompt('Enter triangle height');
+triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
